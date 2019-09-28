9a608f05898ec2c91503cfab5c998d8bfb001c1c
Dean <dean.zhang@shopee.com>
2019-09-29 00:05:08 +0800

    update

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/airdb/bbs-api
 go 1.13
 
 require (
-	github.com/airdb/sailor v0.0.0-20190927151135-86071c5e41c1
+	github.com/airdb/sailor v0.0.0-20190928155953-2e561e7f0131
 	github.com/gin-gonic/gin v1.4.0
 	github.com/jinzhu/gorm v1.9.11
 )
--- mobel/vo/article.go
+++ mobel/vo/article.go
@@ -1,5 +1,7 @@
 package vo
 
+import "github.com/airdb/bbs-api/mobel/po"
+
 type ListArticleReq struct {
 }
 
@@ -12,4 +14,17 @@ type QueryArticleReq struct {
 
 type QueryArticleResp struct {
 	Article []*string `json:"Article"`
+}
+
+type QQRobotQueryReq struct {
+	QQ      uint   `form:"qq"`
+	Group   uint   `form:"group"`
+	Command string `form:"command"`
+	Message string `form:"message"`
+}
+
+type QQRobotQueryResp string
+
+func QueryBBSByKeyword(keyword string) []*po.Article {
+	return po.QueryBBSByKeywords(keyword)
 }
