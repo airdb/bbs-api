4089bd67451099dd79d73a84f02bdf24dcb860d6
YiNo <dean@airdb.com>
2019-10-23 23:39:58 +0800

    update

--- model/vo/square.go
+++ model/vo/square.go
@@ -1,1 +1,18 @@
 package vo
+
+
+type ListSquareReq struct {
+
+}
+
+type ListSquareResp struct {
+	ID uint `json:"id"`
+	TagTypeID uint `json:"tag_type_id"`
+	Status int `json:"status"`
+	Sort uint `json:"sort"`
+	CreateUser uint `json:"create_user"`
+	Title string `json:"Title"`
+	Name string `json:"name"`
+	Image string `json:"image"`
+	Flag bool `json:"flag"`
+}
--- web/handlers/square.go
+++ web/handlers/square.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"github.com/airdb/bbs-api/model/vo"
 	"github.com/airdb/sailor/gin/middlewares"
 	"github.com/gin-gonic/gin"
 )
@@ -13,10 +14,15 @@ import (
 // @Produce  json
 // @Param req body vo.ListArticleReq true "Record"
 // @Success 200 {object} vo.ListArticleResp
-// @Router /square/getAll/:pid [get]
+// @Router /square/query [get]
 func QuerySquare(c *gin.Context) {
 	middlewares.SetResp(
 		c,
-		"list article",
+		vo.ListSquareResp{
+			ID: 3,
+			Name:"前端",
+			Title: "前端开发",
+			Image: "https://www.lyh.red/file/首页轮播_20190418155210_g6fk/20190418160520_8hee.png",
+		},
 	)
 }
