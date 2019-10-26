6909f34bd2c6cd09b62ee05ce5a850e725267740
YiNo <dean@airdb.com>
2019-10-26 12:06:44 +0800

    update

--- web/handlers/square.go
+++ web/handlers/square.go
@@ -24,18 +24,19 @@ func ListSquare(c *gin.Context) {
 		c,
 		enum.AirdbSuccess,
 		vo.ListSquareResp{
-			ID:    3,
-			TagTypeID: 2,
-			Sort: 1,
-			Status: 1,
+			ID:         3,
+			TagTypeID:  2,
+			Sort:       1,
+			Status:     1,
 			CreateUser: 2,
-			Flag: 1,
-			Name:  "前端",
-			Title: "前端开发",
-			Image: "https://www.lyh.red/file/首页轮播_20190418155210_g6fk/20190418160520_8hee.png",
+			Flag:       1,
+			Name:       "前端",
+			Title:      "前端开发",
+			Image:      "https://www.lyh.red/file/首页轮播_20190418155210_g6fk/20190418160520_8hee.png",
 		},
 	)
 }
+
 /*
 
 id: 3,
@@ -52,7 +53,7 @@ flag: 1,
 name: "前端",
 image: "https://www.lyh.red/file/首页轮播_20190418155210_g6fk/20190418160520_8hee.png",
 title: "前端开发"
- */
+*/
 
 // QuerySquare godoc
 // @Summary Query square
